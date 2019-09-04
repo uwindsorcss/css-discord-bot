261f3a420b12f553a5eab88ce4f1da2b30ad691d
Eric Pickup <pickup@uwindsor.ca>
2019-09-03 23:56:53 -0400

    Added more details to warning message

--- main.rb
+++ main.rb
@@ -142,7 +142,7 @@ class Main
 
   def self.command_sent_as_direct_message_to_bot?(event)
     if event.server.nil?
-      return_error(event.user.pm, "This command can only be used in the Discord server.")
+      return_error(event.user.pm, "This command can only be used in the Discord server. Try sending this command in the #bot-commands channel in the CSS server.")
       return true
     end
     return false
