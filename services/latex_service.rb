2e1bd236c2caff0b2ea024bc47f23100cd2bebb8
prairir <me@ryanprairie.com>
2020-03-09 00:08:24 -0400

    file editing system works, i changed it this way so less possibility to have remote code execution. remote code execution can come from the fact that the message goes directly into the command and even if some chars are escaped, i guarentee some person would find a way out of the container that is the command, the compilation would fail but a command outside of the original command, making that a huge security problem. with this system it should be more secure because it actually writes to a file and so there is no chance to escape the command container, hopefully this is enough to make it fairly secure

--- services/latex_service.rb
+++ services/latex_service.rb
@@ -1,5 +1,6 @@
 class LatexService
 
+=begin
   def self.render(message)
     if message.strip.length == 1 or message.strip == '\\' * message.length
       return false
@@ -10,15 +11,42 @@ class LatexService
     system("pdflatex \"\\def\\formula{ #{message} }\\input{formula.tex}\" ") and system("convert -density 300 formula.pdf -alpha off -quality 90 +profile \"*\" formula.png")
 
   end
+=end
 
-  def self.cleanup()
-    
+  def self.render(message, path, file)
+    # stripping it so you cant just put in one letter or a string of backslashs
+    if message.strip.length == 1 || message.strip == '\\' * message.length
+      return false
+    end
+
+    # reading the template file
+    template = File.read(File.join(path, 'template.tex'))
+
+    File.open(File.join(path, file + '.tex'), 'w'){ |outfile|
+      changed = template.gsub('__DATA__', message)
+      outfile.puts changed
+    }
+    true
   end
 
+  # deletes the extra files
+  def self.cleanup(path, file)
+    # these are the files that are created
+    file_endings = ['.aux', '.log', '.pdf', '.png', '.tex']
+    file_endings.each{ |fending|
+      File.delete(path + file + fending) if File.exist?(path + file + fending)
+    }
+  end
+
+  # sanitizes the message by putting a backslash in front of some chars
   def self.sanitize(message)
-    resChars = ['$','\\', '"']
-    resChars.each{ |res|
-      message.gsub!(res, "\\#{res}")
+    # each symbol is a different one that could cause problems
+    # $ is to enter/exit math mode which would cause compilation problems
+    # \\ is obvious
+    # " could cause an escape of the latex function
+    res_chars = ['$', '\\', '"']
+    res_chars.each{ |res|
+      message = message.gsub(res, "\\#{res}")
     }
     message
   end
