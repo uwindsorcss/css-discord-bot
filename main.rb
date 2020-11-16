c85ff2b66545e9f5d20415df8fd4816378e31299
Ryan Prairie <me@ryanprairie.com>
2020-11-16 17:27:05 -0500

    featurization for event roles

--- main.rb
+++ main.rb
@@ -2,9 +2,15 @@ require 'discordrb'
 require 'pry'
 require 'hocon'
 require 'fuzzystringmatch'
+
+# services
 require_relative 'services/discord_message_sender'
 require_relative 'services/building_service'
 require_relative 'services/latex_service'
+
+# modules
+require_relative 'modules/eventRoles'
+
 
 class Main
   CONFIG = Hocon.load("config.conf")
@@ -182,76 +188,12 @@ class Main
     event.message.delete
   end
 
-  # event roles channel add role system
-  # 
-  # event messages will have a simple structure
-  # first line is the role
-  # then a buffer line
-  # then a description of the event
-  # example: 
-  # @name
-  # 
-  # this event exists and some other stuff
-  
-  # this is the first function to add a reaction if the message fits that format
-  bot.message(in: "#event-roles") do |event|
-    role = event.message.role_mentions.first
-    
-    # if role isnt nil and role isnt in EXCLUDE_ROLES
-    # !(a || b) == (!a && !b)
-    unless role.nil? || EXCLUDE_ROLES.include?(role.name)
-      event.message.react("✅")
-    end
+  # event roles featurization
+  if CONFIG["features"]["eventRoles"]
+    bot.include! EventRoles
   end
 
-  # function to add a role to someone who clicked it
-  # makes sure its the right emote
-  bot.reaction_add(emoji: "✅") do |event|
-    role = event.message.role_mentions.first
-    channel = event.message.channel
-    server = channel.server
-    member = server.members.find { |member| member.id == event.user.id }
-
-    # split these up to read easier
-
-    # if channel name is "event-roles" and the user isnt a bot
-    if channel.name == "event-roles" && !event.user.bot_account?
-      # if role isnt nil and role isnt in EXCLUDE_ROLES
-      # !(a || b) == (!a && !b)
-      unless role.nil? || EXCLUDE_ROLES.include?(role.name)
-        # if the user doesnt have the role
-        unless member.role?(role)
-          member.add_role(role)
-        end
-      end
-    end
-  end
-
-  # function to remove a role from someone who unreacted
-  # makes sure its the right emote
-  bot.reaction_remove(emoji: "✅") do |event|
-
-    role = event.message.role_mentions.first
-    channel = event.message.channel
-    server = channel.server
-    member = server.members.find { |member| member.id == event.user.id }
-
-    # if user isnt a bot and its in channel "event-roles"
-    if !event.user.bot_account? && channel.name == "event-roles"
-      # if role isnt nil and role isnt in EXCLUDE_ROLES
-      # !(a || b) == (!a && !b)
-      unless role.nil? || EXCLUDE_ROLES.include?(role.name)
-      # if the user has the role
-      # then remove
-        if member.role?(role)
-          member.remove_role(role)
-        end
-      end
-    end
-  end
-
-  # end of functions relating to event-roles
-
+ 
   def self.command_sent_as_direct_message_to_bot?(event)
     if event.server.nil?
       return_error(event.user.pm, "This command can only be used in the Discord server. Try sending this command in the #bot-commands channel in the CSS server.")
---
+++ modules/eventRoles.rb
@@ -0,0 +1,86 @@
+require 'discordrb'
+
+module EventRoles
+  extend Discordrb::EventContainer
+  
+  EXCLUDE_ROLES = [
+    "Bot",
+    "Admin",
+    "Moderator",
+    "CSS President",
+    "CSS Board Executive",
+    "CSS Board Head",
+    "CSS Board Member",
+  ]
+
+  # event roles channel add role system
+  # 
+  # event messages will have a simple structure
+  # first line is the role
+  # then a buffer line
+  # then a description of the event
+  # example: 
+  # @name
+  # 
+  # this event exists and some other stuff
+  
+  # this is the first function to add a reaction if the message fits that format
+  message(in: "#event-roles") do |event|
+    role = event.message.role_mentions.first
+    
+    # if role isnt nil and role isnt in EXCLUDE_ROLES
+    # !(a || b) == (!a && !b)
+    unless role.nil? || EXCLUDE_ROLES.include?(role.name)
+      event.message.react("✅")
+    end
+  end
+
+  # function to add a role to someone who clicked it
+  # makes sure its the right emote
+  reaction_add(emoji: "✅") do |event|
+    role = event.message.role_mentions.first
+    channel = event.message.channel
+    server = channel.server
+    member = server.members.find { |member| member.id == event.user.id }
+
+    # split these up to read easier
+
+    # if channel name is "event-roles" and the user isnt a bot
+    if channel.name == "event-roles" && !event.user.bot_account?
+      # if role isnt nil and role isnt in EXCLUDE_ROLES
+      # !(a || b) == (!a && !b)
+      unless role.nil? || EXCLUDE_ROLES.include?(role.name)
+        # if the user doesnt have the role
+        unless member.role?(role)
+          member.add_role(role)
+        end
+      end
+    end
+  end
+
+  # function to remove a role from someone who unreacted
+  # makes sure its the right emote
+  reaction_remove(emoji: "✅") do |event|
+
+    role = event.message.role_mentions.first
+    channel = event.message.channel
+    server = channel.server
+    member = server.members.find { |member| member.id == event.user.id }
+
+    # if user isnt a bot and its in channel "event-roles"
+    if !event.user.bot_account? && channel.name == "event-roles"
+      # if role isnt nil and role isnt in EXCLUDE_ROLES
+      # !(a || b) == (!a && !b)
+      unless role.nil? || EXCLUDE_ROLES.include?(role.name)
+      # if the user has the role
+      # then remove
+        if member.role?(role)
+          member.remove_role(role)
+        end
+      end
+    end
+  end
+
+  # end of functions relating to event-roles
+
+end
