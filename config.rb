e489f14dd79acda6f53e8ae0c5ebc6305bec79fe
Ryan Prairie <me@ryanprairie.com>
2021-02-15 15:02:04 -0500

    config for prompt

--- config.rb
+++ config.rb
@@ -10,6 +10,7 @@ module Config
   API_TOKEN = CONFIG["api_token"]
   API_CLIENT_ID = CONFIG["api_client_id"]
   PREFIX = CONFIG["prefix"]
+  PROMPT_CHANNEL = CONFIG["prompt_channel"]
   IMAGE_DIRECTORY_URL = CONFIG["urls"]["image_directory_url"]
   LATEX_DIRECTORY_RELATIVE_PATH = "tmp"
   BOT_USER_ID = CONFIG["bot_user_id"]
