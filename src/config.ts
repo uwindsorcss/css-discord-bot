f0710887072f2ef67267fd47a992be70c1bc444c
Ryan Prairie <me@ryanprairie.com>
2021-12-07 17:11:53 -0500

    Add logger, Add basic config, Add sync log loading
    Add logger, A centalized logger like this lets us setup information we
    require to be logged because we use structured logging. Add config, the
    config type gives us the ability to more fine tune the config in code as
    well as in the config file. The Load config done this way decides the
    log level and a few other things. Add start function in index, this is
    done so we can syncronously read the config in while it being async. Its
    a dumb byproduct of async await.

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,41 @@
-import fs from "fs";
+import {readFile} from "fs/promises";
 import yaml from "js-yaml";
+import {logger} from "./logger";
+
+type ConfigType = {
+  api_token: string;
+  api_client_id: string;
+  bot_user_id: number;
+  debug: boolean;
+  self_roles_channel: string;
+  urls: {
+    mc_address_url: string;
+  };
+  prompt: {
+    channel: string;
+    top_text: string;
+    bottom_text: string;
+  };
+  features: {
+    year: boolean;
+    purge: boolean;
+    equation: boolean;
+    whereis: boolean;
+    selfRoles: boolean;
+    say: boolean;
+    prompt: boolean;
+    jail: boolean;
+    train: boolean;
+  };
+};
+
+let Config: null | ConfigType = null;
+
+const LoadConfig = async (file: string) => {
+  const data = yaml.load(await readFile(file, "utf8"));
+  Config = data as ConfigType;
+  // if Config.debug is set, then set log level to debug, if naw then info
+  logger.level = Config.debug ? "debug" : "info";
+};
+
+export {LoadConfig, Config};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 //import fs from "fs";
 //import {REST} from "@discordjs/rest";
 //import {Routes} from "discord-api-types/v9";
-import pino from "pino";
+import {LoadConfig, Config} from "./config";
+import {logger} from "./logger";
 
-pino().info("hello world");
+const start = async () => {
+  // even though this is inside `src/`, pretend that it isnt
+  await LoadConfig("config.yaml");
+
+  logger.info({Config});
+  /*
+const commandFiles: string[] = fs
+  .readdirSync("./commands")
+  .filter((file: string) => file.endsWith(".ts"));
+
+for (const file of commandFiles) {
+}*/
+};
+
+start();
