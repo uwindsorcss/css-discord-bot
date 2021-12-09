3ff0439822bea06f9d0b9b1a058e2afe867ebbfc
Ryan Prairie <me@ryanprairie.com>
2021-12-09 16:58:42 -0500

    Add event and command dynamic loading
    Add event and command dynamic loading into the bot. As well as bot setup
    stuff. This is untested but _probably_ works

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,100 @@
-import fs from "fs";
 import path from "path";
-//import {REST} from "@discordjs/rest";
+import fs from "fs";
+import {Client, Intents, Collection} from "discord.js";
 //import {Routes} from "discord-api-types/v9";
-import {LoadConfig, Config, ConfigType} from "./config";
+import {LoadConfig, Config} from "./config";
 import {logger} from "./logger";
+import {ClientType, CommandType} from "./types";
 
 const start = async () => {
   // even though this is inside `src/`, pretend that it isnt
   // load in the config
   LoadConfig("config.yaml");
 
-  logger.info({Config});
+  logger.debug({Config});
 
-  // get all the module files
-  const moduleFiles: string[] = fs
-    .readdirSync("src/modules")
-    .filter((file: string) => file.endsWith(".ts"));
+  // create client as ClientType
+  const client: ClientType = new Client({
+    intents: [Intents.FLAGS.GUILDS],
+  }) as ClientType;
 
-  // enumerate through the module files
-  // and load them in
-  for (const feature of Object.keys((Config as any)?.features)) {
+  client.commands = new Collection();
+
+  // return a string array of file names
+  // where the file name ends in `.ts` and it is enabled
+  // in featurization
+  const commandFiles: string[] = fs.readdirSync("./src/commands/").filter(
+    (file: string) =>
+      file.endsWith(".ts") && (Config as any)?.features[file.slice(0, -3)] // slice to get rid of extension
+  );
+
+  // dynamically import and load commands
+  for (const file of commandFiles) {
     const filePath = path.format({
-      root: "./modules/",
-      name: feature,
+      root: "./commands/",
+      name: file,
     });
 
-    if ((Config as any).features[feature]) {
-      const name = await import(filePath);
-      (name as any).something();
+    // actual dynamic import
+    const command: CommandType = (await import(
+      filePath.slice(0, -3)
+    )) as CommandType;
+
+    logger.debug(`Load command file ${filePath}`);
+
+    // load into commands map
+    client.commands.set(command.data.name, command);
+  }
+
+  // array of event files
+  // where the files end in `.ts` and are enabled in featurization
+  const eventFiles: string[] = fs.readdirSync("./src/events/").filter(
+    (file: string) =>
+      file.endsWith(".ts") && (Config as any)?.features[file.slice(0, -3)] // slice to get rid of extension
+  );
+
+  // event loader
+  for (const file of eventFiles) {
+    const filePath = path.format({
+      root: "./events/",
+      name: file,
+    });
+
+    logger.debug(`Load event file ${filePath}`);
+    const event = await import(filePath.slice(0, -3));
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
     }
   }
+
+  // Bot ready event
+  client.once("ready", () => {
+    logger.info("Bot is ready");
+  });
+
+  // command dispatcher
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      logger.error(error);
+      return interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  });
+
+  // login the client
+  client.login(Config?.api_token);
 };
 
 start();
