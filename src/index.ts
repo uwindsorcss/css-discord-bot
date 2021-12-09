c99fd525654250b649facb4259837fb1dfc700fe
Ryan Prairie <me@ryanprairie.com>
2021-12-09 14:52:58 -0500

    Add base for dynamic feature loading
    The dynamic feature loading is finished except it isnt plugged into discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
-//import fs from "fs";
+import fs from "fs";
+import path from "path";
 //import {REST} from "@discordjs/rest";
 //import {Routes} from "discord-api-types/v9";
-import {LoadConfig, Config} from "./config";
+import {LoadConfig, Config, ConfigType} from "./config";
 import {logger} from "./logger";
 
 const start = async () => {
   // even though this is inside `src/`, pretend that it isnt
-  await LoadConfig("config.yaml");
+  // load in the config
+  LoadConfig("config.yaml");
 
   logger.info({Config});
-  /*
-const commandFiles: string[] = fs
-  .readdirSync("./commands")
-  .filter((file: string) => file.endsWith(".ts"));
 
-for (const file of commandFiles) {
-}*/
+  // get all the module files
+  const moduleFiles: string[] = fs
+    .readdirSync("src/modules")
+    .filter((file: string) => file.endsWith(".ts"));
+
+  // enumerate through the module files
+  // and load them in
+  for (const feature of Object.keys((Config as any)?.features)) {
+    const filePath = path.format({
+      root: "./modules/",
+      name: feature,
+    });
+
+    if ((Config as any).features[feature]) {
+      const name = await import(filePath);
+      (name as any).something();
+    }
+  }
 };
 
 start();
---
+++ src/modules/prompt.ts
@@ -0,0 +1,7 @@
+import {logger} from "../logger";
+
+const something = () => {
+  logger.info("hello from prompt");
+};
+
+export {something};
