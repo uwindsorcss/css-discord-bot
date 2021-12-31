92631e7db5d40cba7e04427db6d8b9d4b860ee1e
Ryan Prairie <me@ryanprairie.com>
2021-12-31 11:01:29 -0500

    Add: purge command
    Purge command deletes the last N messages. It takes N as the number of
    messages to delete and then deletes them. It only works for threads and
    text channels in servers.

--- src/commands/pong.ts
+++ src/commands/pong.ts
@@ -1,12 +1,11 @@
 import {CommandType} from "../types";
 import {logger} from "../logger";
 import {SlashCommandBuilder} from "@discordjs/builders";
-import {CommandInteraction, Interaction, CacheType} from "discord.js";
+import {CommandInteraction, CacheType} from "discord.js";
 
 const pongModule: CommandType = {
   data: new SlashCommandBuilder().setName("pong").setDescription("Ping. Pong?"),
   execute: async (interaction: CommandInteraction<CacheType>) => {
-    //execute: async (interaction: Interaction<CacheType>) => {
     logger.info("PONG PONG PONG");
     await interaction.reply("PONG");
   },
--- src/commands/purge.ts
+++ src/commands/purge.ts
@@ -1,14 +1,50 @@
 import {CommandType} from "../types";
 import {logger} from "../logger";
-import {SlashCommandBuilder} from "@discordjs/builders";
+import {
+  SlashCommandBuilder,
+  SlashCommandIntegerOption,
+} from "@discordjs/builders";
+import {
+  CommandInteraction,
+  CacheType,
+  TextChannel,
+  ThreadChannel,
+} from "discord.js";
 
 const purgeModule: CommandType = {
   data: new SlashCommandBuilder()
-    .setName("hello")
-    .setDescription("print hello"),
-  execute: async () => {
-    logger.info("hello");
+    .setName("purge")
+    .setDescription("Purges the last N messages where 1 <= n <= 100")
+    .addIntegerOption((option: SlashCommandIntegerOption) =>
+      option
+        .setName("n")
+        .setDescription("Number of messages to purge")
+        .setRequired(true)
+    ),
+  execute: async (interaction: CommandInteraction<CacheType>) => {
+    let n = interaction.options.getInteger("n");
+    logger.debug(`Purge was called with ${n}`);
+    if (!n) {
+      await interaction.reply("**ERROR**n has to be 1 <= n <= 100");
+      return;
+    }
+    if (n < 1 || n > 100) {
+      await interaction.reply("**ERROR:**n has to be 1 <= n <= 100");
+      return;
+    }
+
+    // if no guild then in DM
+    if (!interaction.guild) {
+      await interaction.reply("**ERROR:** command only works in servers");
+      return;
+    }
+
+    // cast channel to either TextChannel or ThreadChannel
+    const channel = interaction.channel as TextChannel | ThreadChannel;
+
+    // bulk delete (n + 1) number of messages
+    await channel?.bulkDelete(n + 1);
   },
 };
 
-export default purgeModule;
+export {purgeModule as command};
