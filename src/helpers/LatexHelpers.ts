1991fe63c66df29b3294a1ac5bd6d5734751882e
Thang Tran <phucthangvt1101@gmail.com>
2022-07-24 11:15:47 -0400

    Remove unused EquationRender function

--- src/helpers/LatexHelpers.ts
+++ src/helpers/LatexHelpers.ts
@@ -19,19 +19,5 @@ import katex from 'katex';
 import * as htmlToImage from 'html-to-image';
 
 export const EquationRender = (message: string) => {
-    let outputs: string = katex.renderToString(message, {
-        displayMode: true,
-        output: 'html'
-    }) as string;
-    console.log({ outputs })
-    htmlToImage.toPng(outputs as unknown as HTMLElement)
-        .then(function (dataUrl) {
-            var img = new Image();
-            img.src = dataUrl;
-            document.body.appendChild(img);
-        })
-        .catch(function (error) {
-            console.error('oops, something went wrong!', error);
-        });
-    //
+    
 }
