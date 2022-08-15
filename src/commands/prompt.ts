730125aa621d385660440b08c3a47d3c9bb58fd3
Thang99 <tran85@uwindsor.ca>
2022-08-14 22:09:36 -0400

    added prompt

--- src/commands/prompt.ts
+++ src/commands/prompt.ts
@@ -1,14 +1,40 @@
-import {logger} from "../logger";
-import {SlashCommandBuilder} from "@discordjs/builders";
-import {CommandInteraction, CacheType} from "discord.js";
-import {CommandType} from "../types";
+import { logger } from "../logger";
+import { inlineCode, SlashCommandBuilder, SlashCommandStringOption } from "@discordjs/builders";
+import { CommandInteraction, CacheType, TextChannel, MessageEmbed } from "discord.js";
+import { CommandType } from "../types";
+import { Config } from "../config";
 
 const promptModule: CommandType = {
-  data: new SlashCommandBuilder().setName("prompt").setDescription("Prompt?"),
+  data: new SlashCommandBuilder()
+    .setName("prompt")
+    .setDescription("Ask something?")
+    .addStringOption((opt: SlashCommandStringOption) =>
+      opt
+        .setName("question")
+        .setDescription("What is the question?")
+        .setRequired(true)
+    ),
   execute: async (interaction: CommandInteraction<CacheType>) => {
-    logger.info("hello from prompt");
-    await interaction.reply("hello from prompt");
+
+    let channelId = interaction.guild?.channels.cache.find(channel => channel.name === Config?.prompt.channel) as TextChannel
+
+    let question = interaction.options.getString("question")!;
+
+    let promptMess = Config?.prompt.top_text + question + Config?.prompt.bottom_text
+
+    channelId?.send({ content: promptMess });
+
+    const embed = new MessageEmbed()
+      .setTitle("Successfully Ask Question")
+      .setDescription(
+        `Successfully ask ${inlineCode(question)} in ${inlineCode(
+          channelId.name
+        )}!`
+      );
+
+    const ephemeral = true;
+    interaction.reply({ embeds: [embed], ephemeral });
   },
 };
 
-export {promptModule as command};
+export { promptModule as command };
