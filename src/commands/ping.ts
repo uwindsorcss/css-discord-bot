cbc36578b703b513c3996f5eb2ae6cc54c45d08a
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-07-30 03:24:12 -0400

    feat: Ping Stats - The ping command now returns stats like API latency, bot latency, and bot uptime

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,15 +1,46 @@
 import {CommandType} from "../types";
-import {logger} from "../logger";
 import {SlashCommandBuilder} from "@discordjs/builders";
-import {CommandInteraction, CacheType} from "discord.js";
+import {CommandInteraction, CacheType, MessageEmbed} from "discord.js";
 
 const pongModule: CommandType = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Check if bot is alive"),
+    .setDescription("Pings the bot and gets the latency"),
   execute: async (interaction: CommandInteraction<CacheType>) => {
-    logger.info("Pong");
-    await interaction.reply("Pong");
+    const uptime = interaction.client.uptime as number;
+
+    const days = Math.floor(uptime / 86400000);
+    const hours = Math.floor(uptime / 3600000) % 24;
+    const minutes = Math.floor(uptime / 60000) % 60;
+    const seconds = Math.floor(uptime / 1000) % 60;
+    const milliseconds = uptime % 1000;
+
+    const timeComponents: string[] = [];
+    if (days) timeComponents.push(`${days}d`);
+    if (hours) timeComponents.push(`${hours}h`);
+    if (minutes) timeComponents.push(`${minutes}m`);
+    if (seconds) timeComponents.push(`${seconds}s`);
+    if (milliseconds) timeComponents.push(`${milliseconds}ms`);
+
+    const responseEmbed = new MessageEmbed()
+      .setColor("BLUE")
+      .setTitle("Pong")
+      .addFields(
+        {
+          name: ":signal_strength: API Latency",
+          value: `${Math.round(interaction.client.ws.ping)}ms`,
+        },
+        {
+          name: ":signal_strength: Bot Latency",
+          value: `${Math.round(Date.now() - interaction.createdTimestamp)}ms`,
+        },
+        {
+          name: ":arrow_up: Bot Uptime",
+          value: timeComponents.join(", "),
+        }
+      );
+
+    return interaction.reply({embeds: [responseEmbed]});
   },
 };
 
