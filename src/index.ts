5eb4d576f1f91636001885371b18c94bba65bae5
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-08-03 23:14:43 -0400

    feat: Graceful Shutdown

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import {LoadConfig, Config, logger} from "@/config";
 import {ClientType} from "./types";
 import events from "./events";
 import commands from "./commands";
+import process from "process";
+
+const shutDown = async () => {
+  await client.destroy();
+  logger.info("Gracefully shutting down...");
+  process.exit(0);
+};
+
+// Gracefully exit on SIGINT and SIGTERM
+process.on("SIGINT", shutDown);
+process.on("SIGTERM", shutDown);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
