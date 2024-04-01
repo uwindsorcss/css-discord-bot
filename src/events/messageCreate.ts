b8b1f80efe826ce3debdd4b0dda644cf0bc0b8a2
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2024-03-31 23:56:50 -0400

    feat: April Fools General Chat Message Reaction

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,24 @@
+import { Config } from "@/config";
+import {
+  Events,
+  Client,
+  Message,
+  GuildTextBasedChannel,
+} from "discord.js";
+
+module.exports = {
+  name: Events.MessageCreate,
+  async execute(client: Client, message: Message) {
+    try {
+      if (message.author.bot || !Config.features.aprilfools) return;
+
+      // april fools, react with skull emoji to every message in general channel
+      const channel = message.channel as GuildTextBasedChannel;
+      if (channel.name === "general") await message.react("💀");
+      
+    } catch (error) {
+      console.error("Something went wrong when fetching the message:", error);
+      return;
+    }
+  },
+};
