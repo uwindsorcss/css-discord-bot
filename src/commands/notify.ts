7d04e817b606adfade4521a2cfa1f4e30787ac00
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2024-09-27 21:09:08 -0400

    fix: Ensure Null and Undefined Safety in Notify Command

--- src/commands/notify.ts
+++ src/commands/notify.ts
@@ -1,11 +1,11 @@
-import {CommandType} from "../types";
-import {logger} from "@/config";
+import { logger } from "@/config";
 
 import {
   CacheType,
   SlashCommandBuilder,
   GuildMember,
   ChatInputCommandInteraction,
+  Role,
 } from "discord.js";
 
 const notifyModule: CommandType = {
@@ -39,28 +39,48 @@ const notifyModule: CommandType = {
         )
     ),
   execute: async (interaction: ChatInputCommandInteraction<CacheType>) => {
+    if (!interaction.member || !(interaction.member instanceof GuildMember)) {
+      return;
+    }
+
     const subCommand = interaction.options.getSubcommand();
     const student = interaction.member as GuildMember;
     const courseCode = interaction.options.getString("course");
-    let courseSubject = courseCode.slice(0, 4);
 
-    // This variable forces the format to be like *Comp*
+    if (!courseCode) {
+      await interaction.reply({
+        content: "Course code is missing.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    let courseSubject = courseCode.slice(0, 4);
     courseSubject =
       courseSubject.charAt(0).toUpperCase() +
       courseSubject.toLowerCase().slice(1);
     const courseNumber = courseCode.slice(4);
 
     try {
-      const courseRole = interaction.guild.roles.cache.find(
+      if (!interaction.guild) {
+        throw new Error("Guild not found.");
+      }
+
+      const courseRole: Role | undefined = interaction.guild.roles.cache.find(
         (role) => role.name === `${courseSubject}-${courseNumber}`
       );
+
+      if (!courseRole) {
+        throw new Error(`Role not found: ${courseSubject}-${courseNumber}`);
+      }
+
       if (subCommand === "add") {
         await student.roles.add(courseRole);
         await interaction.reply({
           content: `You are now notified for **${courseSubject.toUpperCase()}-${courseNumber}** updates.`,
           ephemeral: true,
         });
-      } else {
+      } else if (subCommand === "remove") {
         await student.roles.remove(courseRole);
         await interaction.reply({
           content: `You are no longer notified for **${courseSubject.toUpperCase()}-${courseNumber}** updates.`,
@@ -68,13 +88,13 @@ const notifyModule: CommandType = {
         });
       }
     } catch (error: any) {
-      logger.info(`Invalid Role: ${courseSubject}-${courseNumber}`);
+      logger.info(`Error handling course role: ${error.message}`);
       await interaction.reply({
-        content: "Invalid course code!",
+        content: "An error occurred or the course code is invalid.",
         ephemeral: true,
       });
     }
   },
 };
 
-export {notifyModule as command};
+export { notifyModule as command };
