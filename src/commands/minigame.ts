d458d39b37e777bb8013dc369c6e08ef3f7a2e0d
Ali <alialm05@proton.me>
2025-02-08 02:07:12 -0500

    game logic complete - rough

--- src/commands/minigame.ts
+++ src/commands/minigame.ts
@@ -10,13 +10,20 @@ import {
   Message,
   Colors,
   ComponentType,
+  Collection,
 } from "discord.js";
+
+// game config
+const WORDBOMB_TURN_TIME = 20_000 // in ms
+const JOIN_TIME = 8_000
+
+// hello
 
 function validateWord(word: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
         readFile("data/words.txt", (error, text) => {
             if (error) {
-                logger.log(error);
+                logger.debug(error);
                 reject(error);
                 return;
             }
@@ -29,6 +36,7 @@ function validateWord(word: string): Promise<boolean> {
                 let mid: number = Math.floor((low + high) / 2);
                 let currentWord = words[mid];
                 if (currentWord === word) {
+                    logger.info("found word: " + currentWord)
                     resolve(true);
                     return;
                 }
@@ -50,7 +58,7 @@ const minigameModule: CommandType = {
     data: new SlashCommandBuilder()
         .setName("minigame")
         .setDescription("Play a minigame in the channel")
-        .addSubcommand((game) =>
+        .addSubcommand((game : any) =>
             game
             .setName("wordbomb")
             .setDescription("Find as much words as possible before time is up!")
@@ -64,6 +72,7 @@ const minigameModule: CommandType = {
         }
 
         const subcommand = interaction.options.getSubcommand();
+        const {channel} = interaction
 
         console.log(subcommand);
 
@@ -81,7 +90,8 @@ const minigameModule: CommandType = {
 
             type Player = {
                 Member: GuildMember,
-                Chances: Number, // # number of chances until eliminated
+                Chances: number, // # number of chances until eliminated
+                CorrectGuess: Boolean,
             }
 
             let players: Array<Player> = []
@@ -97,29 +107,30 @@ const minigameModule: CommandType = {
                     }
                 })
                 .catch((error) => {
-                    logger.log(error);
+                    logger.debug(error);
                 })
             
             const collector = interaction.channel.createMessageComponentCollector(
                 { 
                 componentType: ComponentType.Button,
                 //filter: (i) => i.user,
-                time: 7_000,    
+                time: JOIN_TIME,    
                 }
             )
 
-            collector.on("collect", async (i) => {
+            collector.on("collect", async (i : ChatInputCommandInteraction) => {
 
                 if (players.find(p => p.Member.id === i.member.id)) {
-                    return await i.reply({ content: "You are already in the game!" });
+                    return await i.reply({ content: "You are already in the game!", ephemeral: true});
                 }
 
                 players.push({
                     Member: i.member as GuildMember,
-                    Chances: 2
+                    Chances: 2,
+                    CorrectGuess: false
                 } as Player);
 
-                await i.reply({ content: "You have joined the game!" });
+                await i.reply({ content: "You have joined the game!", ephemeral: true});
             })
 
             const joinButton = new ButtonBuilder()
@@ -134,6 +145,75 @@ const minigameModule: CommandType = {
             );
 
             await interaction.editReply({ embeds: [embed], components: [row] });
+
+            var resolveTurn : any
+            var currentPlayer : Player;
+            var refreshId : number; 
+
+            async function wait(ms : number) : Promise<void> {
+                return new Promise(resolve => {
+                    resolveTurn = resolve
+                    setTimeout(resolve, ms)
+                })
+            }
+
+            setTimeout(async () => {
+
+                // checks for atleast two palyers
+                if (players.length < 2){
+                    interaction.channel.send(`Atleast 2 people are required for this game 😔`)
+                    return
+                }
+                
+                while (players.length > 0){
+                    for (let i in players){
+
+                        currentPlayer = players[i]
+                        channel.send(`**its ${players[i].Member.user} turn!**`)
+                        
+                        let guessedCorrectly : Boolean = false
+
+                        const filter = (msg : Message) => (msg.member.id === players[i].Member.id );
+                        const collector = channel.createMessageCollector({filter, time: WORDBOMB_TURN_TIME});
+                        collector.on('collect', async (msg : Message) => {   
+                            let wordIsValid : Boolean = await validateWord(msg.content)
+                            console.log(`${msg.content} is a valid word? : ${wordIsValid}` )
+                            
+                            if (wordIsValid){
+                                console.log("VALID WORD!")
+                                collector.stop()
+                                resolveTurn()
+                                guessedCorrectly = true
+                                channel.send(`**Correct! 👍**`)
+                            }
+                        });
+                        
+                        await wait(WORDBOMB_TURN_TIME)
+                        if (!guessedCorrectly){
+                            currentPlayer.Chances -= 1
+                            
+                            if (currentPlayer.Chances <= 0) {
+                                const index = players.findIndex(p => p.Member.id == currentPlayer.Member.id)
+
+                                if (index > -1){
+                                    players.splice(index, 1)
+                                }else{
+                                    logger.info("could not find player")
+                                }
+
+                                channel.send(`**No more chances left, you have been Elimnated ❌**`)
+                            
+                            } else{
+                                channel.send(`Times up! 😕\n -1 Chance 👎`)
+                            }
+
+                        }
+
+                    }
+                }
+                interaction.channel.send(`game ended`)
+            }, JOIN_TIME);
+
 
         } else {
             await interaction.editReply({ content: "Invalid game", embeds: [embed] });
